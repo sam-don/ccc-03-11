40e07c120370635c2110d8f53990fa329070b717
Sam Don <71539578+sam-don@users.noreply.github.com>
2020-11-12 12:01:09 +1100

    Added seed books cli command

--- commands.py
+++ commands.py
@@ -12,3 +12,19 @@ def create_db():
 def drop_db():
     db.drop_all()
     print("Tables Deleted")
+
+@db_commands.cli.command("seed")
+def seed_db():
+    from models.Book import Book
+    from faker import Faker
+
+    faker = Faker()
+
+    for i in range(10):
+        book = Book()
+        book.title = faker.catch_phrase()
+        db.session.add(book)
+        print(f"{i+1} book record(s) created")
+
+    db.session.commit()
+    print("Tables seeded")
